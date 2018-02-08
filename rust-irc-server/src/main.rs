51ee39f99460f86cbc8e815d1acbb97eae363472
Krishna Kumar <krishnak@thoughtworks.com>
2018-02-08 08:26:52 +0530

    [Krishna] Client registering, sending message to server, server broadcasting to all clients done, have to store client side received data somewhere

--- rust-irc-server/src/main.rs
+++ rust-irc-server/src/main.rs
@@ -9,9 +9,20 @@ use rocket::State;
 use rand::{thread_rng, Rng};
 use std::sync::{Mutex, Arc};
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate rocket_contrib;
+use rocket_contrib::{Json, Value};
+
+extern crate reqwest;
+extern crate url;
+use url::{Url};
+
+struct ClientId {
+    source_ip: String,
+    session_id: String
+}
 
 struct Register {
-    data_map: Arc<Mutex<HashMap<String, String>>>
+    data_map: Arc<Mutex<HashMap<String, ClientId>>>
 }
 
 #[derive(Deserialize)]
@@ -27,8 +38,8 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/register/<name>")]
-fn register(name: String, register: State<Register>) -> String {
+#[get("/register/<name>/<ip>")]
+fn register(name: String, ip: String, register: State<Register>) -> String {
     println!("register {}",name);
     let mut mutable_data_map = register.data_map.lock().unwrap();
     let found = match mutable_data_map.get(&name) {
@@ -46,16 +57,52 @@ fn register(name: String, register: State<Register>) -> String {
         let mut rng = thread_rng();
         let id = rng.gen::<usize>();
         println!("id generated for {} is {}", name, id);
-        let value = format!("{}",id);
-        mutable_data_map.insert(name, value);
+        let session_id = format!("{}",id);
+        mutable_data_map.insert(name, ClientId{source_ip: ip, session_id: session_id.clone()});
+        session_id
+    } else {
+        "This name is already registered".to_string()
     }
-    "register".to_string()
 }
 
-//#[post("/broadcast",data="<Message>")]
-//fn broadcast_msg(message: Json<Message>) {
-//
-//}
+#[post("/broadcast",data="<message>")]
+fn broadcast_msg(message: Json<Message>, register: State<Register>) {
+    let mutable_data_map = register.data_map.lock().unwrap();
+    let found = match mutable_data_map.get(&message.user_name) {
+        Some(clientId) => {
+            println!("found clientid {};{}", clientId.session_id, clientId.source_ip);
+            println!("received details {};{};{}", message.session_id, message.source_ip, message.user_name);
+            if clientId.session_id == message.session_id
+                && clientId.source_ip == message.source_ip {
+                println!("clientid {};{}", clientId.session_id, clientId.source_ip);
+                true
+            }else {
+                false
+            }
+        },
+        None => {
+            false
+        }
+    };
+
+    if found {
+        println!("sending message to all {}", message.message);
+        for val in mutable_data_map.values() {
+            println!("sending to {}, {}",val.source_ip, val.session_id)
+        }
+        let uri_string = format!("http://localhost:8000/receive/{}",message.message);
+        let uri:Url = uri_string.parse().unwrap();
+        let mut response = reqwest::get(uri).unwrap();
+        println!("send to client {}", response.text().unwrap())
+    }
+}
+
+//Logout implementation
+//Periodic Cache clear
+//get messages
+//get users
+//add message time
+
 
 //fn start_redis() -> Option<simple_redis::client::Client> {
 //    match simple_redis::create("redis://127.0.0.1:6379/") {
@@ -74,6 +121,6 @@ fn main() {
     rocket::ignite()
 //        .manage(start_redis())
         .manage(Register {data_map: Arc::new(Mutex::new(HashMap::new()))})
-        .mount("/", routes![index,register])
+        .mount("/", routes![index,register,broadcast_msg])
         .launch();
 }
--- rust-irc/src/main.rs
+++ rust-irc/src/main.rs
@@ -2,6 +2,23 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate reqwest;
+extern crate url;
+use url::{Url};
+
+use rocket::State;
+use std::sync::{Mutex, Arc};
+use std::collections::HashMap;
+
+struct MetaData {
+    client_data: Arc<Mutex<ClientData>>
+}
+
+struct ClientData {
+    session_id: String,
+    user_name: String,
+    local_ip: String
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -13,6 +30,46 @@ fn hello_user(user: String) -> String {
     format!("Hello, {}!", user)
 }
 
+#[get("/register/<name>")]
+fn register_me(name: String, meta_data: State<MetaData>) -> String {
+    let mut mut_client_data = meta_data.client_data.lock().unwrap();
+    let uri_string = format!("http://localhost:8001/register/{}/127.0.0.1",name);
+    let uri:Url = uri_string.parse().unwrap();
+    let mut response = reqwest::get(uri).unwrap();
+    let session_id = response.text().unwrap();
+    println!("response {}",session_id);
+    mut_client_data.session_id = session_id;
+    mut_client_data.user_name = name;
+    println!("response code {}",response.status());
+    "registered u".to_string()
+}
+
+#[get("/send/<message>")]
+fn send_msg(message: String, meta_data: State<MetaData>) {
+    let client_data = meta_data.client_data.lock().unwrap();
+    let uri_string = format!("http://localhost:8001/broadcast");
+    let uri:Url = uri_string.parse().unwrap();
+
+    let mut map = HashMap::new();
+    map.insert("source_ip", "127.0.0.1");
+    map.insert("user_name", &client_data.user_name);
+    map.insert("session_id", &client_data.session_id);
+    map.insert("message", &message);
+
+    let client = reqwest::Client::new();
+    let res = client.post(uri)
+        .json(&map)
+        .send().unwrap();
+}
+
+#[get("/receive/<message>")]
+fn receive_msg(message: String) {
+    println!("received message {} and storing it locally on client",message);
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index,hello_user]).launch();
+    rocket::ignite()
+        .manage(MetaData{ client_data: Arc::new(Mutex::new(ClientData{session_id:"".to_string(), user_name:"".to_string(), local_ip:"".to_string()}))})
+        .mount("/", routes![index,hello_user,register_me,send_msg,receive_msg])
+        .launch();
 }
